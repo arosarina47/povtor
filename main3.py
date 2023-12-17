98c234ffb0249fe704f984fb8f4d3e62041f3465
arosarina47 <145270087+arosarina47@users.noreply.github.com>
2023-12-17 17:15:49 +0200

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,9 @@
+a = int(input("Введіть перше число"))
+b = int(input("Введіть друге число"))
+c = int(input("Введіть третє число"))
+if a > b and a > c:
+    d = a + b +c
+    print(d)
+else:
+    f = b - c
+    print(f)
